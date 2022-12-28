1036ff6604929b3d175774a987e1bfc3bb1b6049
rambabu <mramunalluri@gmail.com>
2022-12-28 15:21:08 +0530

    add jenkinsfile

--- jenkinsfiles/wordpress.groovy
+++ jenkinsfiles/wordpress.groovy
@@ -18,13 +18,13 @@ pipeline {
 								          
             stage('mysql deployment') {
 			  steps {
-			    sh 'kubectl create -f mysql-user-pass.yaml'
-	            sh 'kubectl create -f mysql-db-url.yaml'
-                sh 'kubectl create -f mysql-root-pass.yaml'
-                sh 'kubectl create -f mysql-pv.yaml'
-                sh 'kubectl create -f mysql-pvc.yaml'
-                sh 'kubectl create -f mysql-deployment.yaml'
-                sh 'kubectl create -f mysql-service.yaml'		         
+			    sh 'kubectl create -f wordpre-phpmysql-mysql-deployments/mysql-user-pass.yaml'
+	            sh 'kubectl create -f wordpre-phpmysql-mysql-deployments/mysql-db-url.yaml'
+                sh 'kubectl create -f wordpre-phpmysql-mysql-deployments/mysql-root-pass.yaml'
+                sh 'kubectl create -f wordpre-phpmysql-mysql-deployments/mysql-pv.yaml'
+                sh 'kubectl create -f wordpre-phpmysql-mysql-deployments/mysql-pvc.yaml'
+                sh 'kubectl create -f wordpre-phpmysql-mysql-deployments/mysql-deployment.yaml'
+                sh 'kubectl create -f wordpre-phpmysql-mysql-deployments/mysql-service.yaml'		         
 	                 }
 			    }
           }
