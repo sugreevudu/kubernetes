43e78a118baa1e2a51c85e2ec7ef62476f0505a3
rambabu <mramunalluri@gmail.com>
2022-12-28 15:14:47 +0530

    add jenkinsfile

--- jenkinsfiles/wordpress.groovy
+++ jenkinsfiles/wordpress.groovy
@@ -12,22 +12,20 @@ pipeline {
 	
                 steps {
 
-	            checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/sugreevudu/kubernetes.git']]])			                   }
-							                           }
-								        }
+	            checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/sugreevudu/kubernetes.git']]])			                   			                           
+                      }
+				   }
 								          
-  stage('mysql deployment') {
-							       steps {
-								                    sh 'kubectl create -f mysql-user-pass.yaml'
-
+            stage('mysql deployment') {
+			  steps {
+			    sh 'kubectl create -f mysql-user-pass.yaml'
 	            sh 'kubectl create -f mysql-db-url.yaml'
-                    sh 'kubectl create -f mysql-root-pass.yaml'
-                    sh 'kubectl create -f mysql-pv.yaml'
-                    sh 'kubectl create -f mysql-pvc.yaml'
-                    sh 'kubectl create -f mysql-deployment.yaml'
-                    sh 'kubectl create -f mysql-service.yaml'		         
-	     }
-									         }
-									    }
-										}
-
+                sh 'kubectl create -f mysql-root-pass.yaml'
+                sh 'kubectl create -f mysql-pv.yaml'
+                sh 'kubectl create -f mysql-pvc.yaml'
+                sh 'kubectl create -f mysql-deployment.yaml'
+                sh 'kubectl create -f mysql-service.yaml'		         
+	                 }
+			    }
+          }
+	}
